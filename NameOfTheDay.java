3dc029de99fcc6ca676dfd2a4b91908d1eeec82f
Kapsel94 <32665455+Kapsel94@users.noreply.github.com>
2018-03-02 18:38:41 +0100

    Add files via upload

--- NameOfTheDay.java
+++ NameOfTheDay.java
@@ -1,114 +1,68 @@
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.stream.IntStream;
 
-/**
- * Created by Kapsel on 2018-02-17.
- */
-public class NameOfTheDay {
 
-    private Scanner reader;
-    private int year,month,day,days;
-    private final int fday=1,fmonth=1,fyear=1900; // pierwszy dzien naliczania
+public class NameOfTheDay{
+    private int year,month,day;
+    private int fyear,fmonth,fday;
+    //private Scanner reader;
 
-    public NameOfTheDay(Scanner reader){
-        this.reader=reader;
-        year=1900;
-        month=1;
-        day=1;
+
+    public NameOfTheDay(){
+        fyear=1;  year=1;
+        fmonth=1;    month=1;
+        fday=1;      day=1;
+        //this.reader=reader;
     }
 
-    public static void main(String[] args){
-        // Assuming that you already have this.
-        int year = 1994;
-        int month = 7;
-        int day = 28;
-        Date date=new Date();
+    public void setYear(Scanner reader){
+        System.out.print("Year: ");
+        String input=reader.nextLine();
 
-// First convert to Date. This is one of the many ways.
-        String dateString = String.format("%d-%d-%d", year, month, day);
-        try{
-            date = new SimpleDateFormat("yyyy-M-d").parse(dateString);
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-
-// Then get the day of week from the Date based on specific locale.
-        String dayOfWeek = new SimpleDateFormat("EEEE", Locale.ENGLISH).format(date);
-
-        System.out.println(dayOfWeek); // Friday
     }
 
-    private void combo(Scanner reader)                     /* rok, miesiac, data w jednym, w tej kolejnosci*/ {
-       year(reader);
-       month(reader);
-       day(reader);
+    public void setMonth(int month) {
+        this.month = month;
     }
-    private void year(Scanner reader)                      /* prosi o podanie roku*/             {
-        while(true){
-            System.out.print("Rok (dolna granica to 1900}:");
-            String input=reader.nextLine();
-        if(input.matches("\\d{4}")){
-            this.year=Integer.parseInt(input);
-            break;
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setDays(Scanner reader) {
+        final int[] thirtyDays = {4, 6, 9, 11};
+        final int[] thirtyOneDays = {1, 3, 5, 7, 8, 10, 12};
+        if (IntStream.of(thirtyOneDays).anyMatch(x -> x == this.month)) {
+            dayScanner(reader, 31);
         }
-        else System.out.println("Niewłaściwy zakres.");
+        else if (IntStream.of(thirtyDays).anyMatch(x -> x == this.month)) {
+            dayScanner(reader, 30);
+        }
+        else {
+            if(this.year%4!=0||(this.year%100==0&&this.year%400!=0))
+                dayScanner(reader, 28);
+            else
+                dayScanner(reader,29);
         }
     }
-    private void month(Scanner reader)                     /* prosi o podanie miesiaca*/{
-        while(true){
-            System.out.print("Miesiąc: ");
-            String input=reader.nextLine();
-            if(input.matches("\\d{1,2}")&&Integer.parseInt(input)<13){
-                this.month=Integer.parseInt(input);
-                break;
+
+    public int getDay() {
+        return this.day;
+    }
+
+    private void dayScanner(Scanner reader, int range){
+        System.out.print("Day: ");
+        String input = reader.nextLine();
+        if((input.matches("\\d{1,2}"))){
+            int day=Integer.parseInt(input);
+            if(day>0&&day<=range){
+                this.day=day;
+                return;
             }
-            else System.out.println("Niewłaściwe dane.");
         }
-    }
-    private void day(Scanner reader)                       /* prosi o podanie dnia */{
-        while(true){
-            System.out.print("Dzień: ");
-            String input=reader.nextLine();
-            if(input.matches("\\d{1,2}")&&((days30()&&Integer.parseInt(input)<31)||(days31()&&Integer.parseInt(input)<32))){
-                this.day=Integer.parseInt(input);
-                break;
-            }
-            else if(input.matches("\\d{1,2}")&&this.month==2){
-                if(notFebruary(input)){
-                    System.out.println("Niewłaściwe dane.");
-                }
-                else{
-                    this.day=Integer.parseInt(input);
-                    return;
-                }
-            }
-            else System.out.println("Niewłaściwe dane.");
-        }
-    }
-    private void days()                                    /* oblicza ile dni uplynelo od 1 stycznia 1900, poniedzielak*/{    }
-    private boolean days31(){
-        ArrayList<Integer> days=new ArrayList<>();
-        int[] day={1,3,5,7,8,10,12};
-        if(Arrays.asList(day).contains(this.month)){
-            return true;
-        }
-        else return false;
-    }
-    private boolean days30(){
-        ArrayList<Integer> days=new ArrayList<>();
-        int[] day={4,6,9,11};
-        if(Arrays.asList(day).contains(this.month)){
-            return true;
-        }
-        else return false;
-    }
-    private boolean notFebruary(String input){
-        if(Integer.parseInt(input)>29||
-                (Integer.parseInt(input)==29&&(this.year%4!=0||(this.year%100==0&&this.year%400!=0)))){
-            return true;
-        }
-        else return false;
+        System.out.println("Wrong day. Try again");
+        dayScanner(reader,range);
     }
 
 
--- NameOfTheDayTest.java
+++ NameOfTheDayTest.java
@@ -1,18 +1,147 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Created by Kapsel on 2018-02-28.
- */
-class NameOfTheDayTest {
-    @BeforeEach
-    void setUp() {
-    }
+public class NameOfTheDayTest {
 
-    @AfterEach
-    void tearDown() {
-    }
 
+    @Test
+    public void isDay31Valid(){
+        String data="awdwda\n40\n-10\n02";
+        NameOfTheDay day=new NameOfTheDay();
+        day.setMonth(1);
+        day.setDays(new Scanner(data));
+        assertEquals(2,day.getDay());
+    }
+    @Test
+    public void isDay31MessageNot(){
+        String data="31";
+        ByteArrayOutputStream mockOut = new ByteArrayOutputStream();
+        PrintStream newOut = new PrintStream(mockOut);
+        System.setOut(newOut);
+        NameOfTheDay day=new NameOfTheDay();
+        day.setMonth(1);
+        day.setDays(new Scanner(data));
+        try{
+            assertFalse(new String(mockOut.toByteArray(), "UTF-8").contains("Wrong day. Try again"));
+        }
+        catch(Exception e){}
+    }
+    @Test
+    public void isDay31Message(){
+        String data="40\n02";
+        ByteArrayOutputStream mockOut = new ByteArrayOutputStream();
+        PrintStream newOut = new PrintStream(mockOut);
+        System.setOut(newOut);
+        NameOfTheDay day=new NameOfTheDay();
+        day.setMonth(1);
+        day.setDays(new Scanner(data));
+        try{
+            assertTrue(new String(mockOut.toByteArray(), "UTF-8").contains("Wrong day. Try again"));
+        }
+        catch(Exception e){}
+    }
+    @Test
+    public void isDay30Valid(){
+        String data="31\n-10\n02";
+        NameOfTheDay day=new NameOfTheDay();
+        day.setMonth(4);
+        day.setDays(new Scanner(data));
+        assertEquals(2,day.getDay());
+    }
+    @Test
+    public void isDay30MessageNot(){
+        String data="30";
+        ByteArrayOutputStream mockOut = new ByteArrayOutputStream();
+        PrintStream newOut = new PrintStream(mockOut);
+        System.setOut(newOut);
+        NameOfTheDay day=new NameOfTheDay();
+        day.setMonth(4);
+        day.setDays(new Scanner(data));
+        try{
+            assertFalse(new String(mockOut.toByteArray(), "UTF-8").contains("Wrong day. Try again"));
+        }
+        catch(Exception e){}
+    }
+    @Test
+    public void isDay30Message(){
+        String data="31\n02";
+        ByteArrayOutputStream mockOut = new ByteArrayOutputStream();
+        PrintStream newOut = new PrintStream(mockOut);
+        System.setOut(newOut);
+        NameOfTheDay day=new NameOfTheDay();
+        day.setMonth(4);
+        day.setDays(new Scanner(data));
+        try{
+            assertTrue(new String(mockOut.toByteArray(), "UTF-8").contains("Wrong day. Try again"));
+        }
+        catch(Exception e){}
+    }
+    @Test
+    public void doesNotFebruary1900Have29Days(){
+        String data="29\n10";
+        NameOfTheDay day=new NameOfTheDay();
+        day.setMonth(2);
+        day.setYear(1900);
+        day.setDays(new Scanner(data));
+        assertNotEquals(29,day.getDay());
+    }
+    @Test
+    public void doesNotFebruary1901Have29Days(){
+        String data="29\n10";
+        NameOfTheDay day=new NameOfTheDay();
+        day.setMonth(2);
+        day.setYear(1901);
+        day.setDays(new Scanner(data));
+        assertNotEquals(29,day.getDay());
+    }
+    @Test
+    public void doesFebruary2000Have29Days(){
+        String data="30\n29";
+        NameOfTheDay day=new NameOfTheDay();
+        day.setMonth(2);
+        day.setYear(2000);
+        day.setDays(new Scanner(data));
+        assertEquals(29,day.getDay());
+    }
+    @Test
+    public void doesFebruary2004Have29Days(){
+        String data="-30\n29\n15";
+        NameOfTheDay day=new NameOfTheDay();
+        day.setMonth(2);
+        day.setYear(2004);
+        day.setDays(new Scanner(data));
+        assertEquals(29,day.getDay());
+    }
+    @Test
+    public void isDay28MessageNot(){
+        String data="28";
+        ByteArrayOutputStream mockOut = new ByteArrayOutputStream();
+        PrintStream newOut = new PrintStream(mockOut);
+        System.setOut(newOut);
+        NameOfTheDay day=new NameOfTheDay();
+        day.setMonth(2);
+        day.setDays(new Scanner(data));
+        try{
+            assertFalse(new String(mockOut.toByteArray(), "UTF-8").contains("Wrong day. Try again"));
+        }
+        catch(Exception e){}
+    }
+    @Test
+    public void isDay28Message(){
+        String data="29\n02";
+        ByteArrayOutputStream mockOut = new ByteArrayOutputStream();
+        PrintStream newOut = new PrintStream(mockOut);
+        System.setOut(newOut);
+        NameOfTheDay day=new NameOfTheDay();
+        day.setMonth(2);
+        day.setDays(new Scanner(data));
+        try{
+            assertTrue(new String(mockOut.toByteArray(), "UTF-8").contains("Wrong day. Try again"));
+        }
+        catch(Exception e){}
+    }
 }
