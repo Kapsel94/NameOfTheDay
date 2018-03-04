ecda4b13134d459c0ba404d4d4d2235e5fd5371e
Kapsel94 <32665455+Kapsel94@users.noreply.github.com>
2018-03-04 13:21:55 +0100

    Add files via upload

--- NOTDLogic.java
+++ NOTDLogic.java
@@ -5,7 +5,7 @@ public class NOTDLogic {
     private final static int DAY=1;
     private int year,month,day;
     //private int daysDiff;
-    private Days daysName;
+    //private Days daysName;
     private static final int[] monthsDays={31,28,31,30,31,30,31,31,30,31,30,31};
 
     public NOTDLogic(int year,int month,int day){
@@ -19,18 +19,17 @@ public class NOTDLogic {
         this.day=day.getDay();
     }
 
-    public int counting()
+    public int howManyDaysDiff()
     {
         boolean isLater=this.year>=YEAR;
         if(isLater){
-            return countingLater();
+            return howManyDaysLater();
         }
-        return 0;
+        else return howManyDaysEarlier();
     }
-    public int countingLater()       //counting number of days since 1st January 1900
+    public int howManyDaysLater()       //howManyDaysDiff number of days since 1st January 1900
     {
         int daysDiff=365*(this.year-YEAR)+howManyLeapYearsLater();
-        int months=this.month-MONTH;
         int days=this.day-DAY;
         for(int i=0;i<this.month-1;i++)
             daysDiff+=monthsDays[i];
@@ -39,10 +38,28 @@ public class NOTDLogic {
             return daysDiff-1;
         return daysDiff;
     }
+    public int howManyDaysEarlier()     //howManyDaysDiff number of days to 1st January 1900
+    {
+        int fyear,fmonth,fday;
+        fyear=1899;
+        fmonth=12;
+        fday=31;
+        int daysDiff=365*(fyear-this.year)+howManyLeapYearsEarlier();
+        int days=fday-this.day;
+        for(int i=fmonth;i>this.month;i--)
+            daysDiff+=monthsDays[i-1];
+        daysDiff+=days;
+        if(this.month==2&&this.day==29)
+            return daysDiff-1;
+        else if(this.month==2)
+            return daysDiff-2;
+        return daysDiff+1;
+    }
     public int howManyLeapYearsLater(){
         int diff=this.year-YEAR;
         if(diff>3) {
-            int notDivisiveBy400 = (((diff + 300) / 400) - 1) * 3 + ((diff + 300) % 400) / 100;
+            //int notDivisiveBy400 = (((diff + 300) / 400) - 1) * 3 + ((diff + 300) % 400) / 100;
+            int notDivisiveBy400=diff/100-(diff+300)/400;
             if (!(this.month > 2 || (this.month == 2 && this.day == 29))&&isLeapYear())
                 return diff / 4 - notDivisiveBy400-1;
             else return diff / 4 - notDivisiveBy400;
@@ -50,25 +67,56 @@ public class NOTDLogic {
         else return 0;
     }
 
-    public String whichDayIsIt(){
-        int day=countingLater()%7;
-        switch(day){
-            case 0:
-                return Days.MONDAY.getName();
-            case 1:
-                return Days.TUESDAY.getName();
-            case 2:
-                return Days.WEDNESDAY.getName();
-            case 3:
-                return Days.THURSDAY.getName();
-            case 4:
-                return Days.FRIDAY.getName();
-            case 5:
-                return Days.SATURDAY.getName();
-            default:
-                return Days.SUNDAY.getName();
+    public int howManyLeapYearsEarlier(){
+        int diff=YEAR-this.year;
+        if(diff>3) {
+            //int notDivisiveBy400 = (((diff + 100) / 400) - 1) * 3 + ((diff + 100) % 400) / 100+2;
+            int notDivisiveBy400=diff/100-(diff+100)/400;
+            if ((this.month > 2 || (this.month == 2 && this.day == 29))&&isLeapYear())
+                return diff / 4 - notDivisiveBy400-1;
+            else return diff / 4 - notDivisiveBy400;
         }
+        else return 0;
     }
+
+    public String whichDayIsIt(){
+        int day= howManyDaysDiff()%7;
+        if(this.year>=YEAR) {
+            switch (day) {
+                case 0:
+                    return Days.MONDAY.getName();
+                case 1:
+                    return Days.TUESDAY.getName();
+                case 2:
+                    return Days.WEDNESDAY.getName();
+                case 3:
+                    return Days.THURSDAY.getName();
+                case 4:
+                    return Days.FRIDAY.getName();
+                case 5:
+                    return Days.SATURDAY.getName();
+                default:
+                    return Days.SUNDAY.getName();
+            }
+        }
+        else
+            switch (day) {
+                case 0:
+                    return Days.MONDAY.getName();
+                case 6:
+                    return Days.TUESDAY.getName();
+                case 5:
+                    return Days.WEDNESDAY.getName();
+                case 4:
+                    return Days.THURSDAY.getName();
+                case 3:
+                    return Days.FRIDAY.getName();
+                case 2:
+                    return Days.SATURDAY.getName();
+                default:
+                    return Days.SUNDAY.getName();
+            }
+        }
     private boolean isLeapYear(){
         int year=this.year;
         if(year%400==0)
--- NOTDLogicTest.java
+++ NOTDLogicTest.java
@@ -1,7 +1,5 @@
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-
 
 class NOTDLogicTest {
 
@@ -15,6 +13,10 @@ class NOTDLogicTest {
         assertEquals(122,logic.howManyLeapYearsLater());
         logic=new NOTDLogic(2413,1,1);
         assertEquals(125,logic.howManyLeapYearsLater());
+        logic=new NOTDLogic(1904,2,27);
+        assertEquals(0,logic.howManyLeapYearsLater());
+        logic=new NOTDLogic(1904,2,29);
+        assertEquals(1,logic.howManyLeapYearsLater());
     }
     @Test
     public void is2100Year49thLeapYear(){
@@ -32,21 +34,36 @@ class NOTDLogicTest {
     @Test
     public void isXDaysLeftSince1stJanuary1900(){
         NOTDLogic logic=new NOTDLogic(1994,1,1);
-        assertEquals(34333,logic.counting());
+        assertEquals(34333,logic.howManyDaysDiff());
         logic=new NOTDLogic(1994,7,28);
-        assertEquals(34541,logic.counting());
+        assertEquals(34541,logic.howManyDaysDiff());
         logic=new NOTDLogic(2004,2,28);
-        assertEquals(38043,logic.counting());
+        assertEquals(38043,logic.howManyDaysDiff());
         logic=new NOTDLogic(2004,2,29);
-        assertEquals(38044,logic.counting());
+        assertEquals(38044,logic.howManyDaysDiff());
         logic=new NOTDLogic(2004,3,1);
-        assertEquals(38045,logic.counting());
+        assertEquals(38045,logic.howManyDaysDiff());
         logic=new NOTDLogic(2096,2,28);
-        assertEquals(71646,logic.counting());
+        assertEquals(71646,logic.howManyDaysDiff());
         logic=new NOTDLogic(2096,2,29);
-        assertEquals(71647,logic.counting());
+        assertEquals(71647,logic.howManyDaysDiff());
         logic=new NOTDLogic(2096,3,1);
-        assertEquals(71648,logic.counting());
+        assertEquals(71648,logic.howManyDaysDiff());
+    }
+    @Test
+    public void isXDaysTo1stJanuary1900(){
+        NOTDLogic logic=new NOTDLogic(1896,3,1);
+        assertEquals(1401,logic.howManyDaysDiff());
+        logic=new NOTDLogic(1896,2,29);
+        assertEquals(1402,logic.howManyDaysDiff());
+        logic=new NOTDLogic(1896,2,28);
+        assertEquals(1403,logic.howManyDaysDiff());
+        logic=new NOTDLogic(1895,2,14);
+        assertEquals(1782,logic.howManyDaysDiff());
+        logic=new NOTDLogic(1894,2,5);
+        assertEquals(2156,logic.howManyDaysDiff());
+        logic=new NOTDLogic(1703,5,25);
+        assertEquals(71809,logic.howManyDaysDiff());
     }
     @Test
     public void isExactlyGoodNameDay(){
@@ -64,7 +81,31 @@ class NOTDLogicTest {
         assertEquals("Day: Monday",logic.whichDayIsIt());
         logic=new NOTDLogic(2016,3,1);
         assertEquals("Day: Tuesday",logic.whichDayIsIt());
+        logic=new NOTDLogic(1894,1,15);
+        assertEquals("Day: Monday",logic.whichDayIsIt());
+        logic=new NOTDLogic(1791,5,3);
+        assertEquals("Day: Tuesday",logic.whichDayIsIt());
+        logic=new NOTDLogic(1703,5,25);
+        assertEquals("Day: Friday",logic.whichDayIsIt());
 
     }
-
+    @Test
+    public void isLeapYearsEarlier(){
+        NOTDLogic logic=new NOTDLogic(1896,3,1);
+        assertEquals(0,logic.howManyLeapYearsEarlier());
+        logic=new NOTDLogic(1896,2,29);
+        assertEquals(0,logic.howManyLeapYearsEarlier());
+        logic=new NOTDLogic(1896,2,28);
+        assertEquals(1,logic.howManyLeapYearsEarlier());
+        logic=new NOTDLogic(1800,3,1);
+        assertEquals(24,logic.howManyLeapYearsEarlier());
+        logic=new NOTDLogic(1800,2,28);
+        assertEquals(24,logic.howManyLeapYearsEarlier());
+        logic=new NOTDLogic(1796,2,28);
+        assertEquals(25,logic.howManyLeapYearsEarlier());
+        logic=new NOTDLogic(1600,3,1);
+        assertEquals(72,logic.howManyLeapYearsEarlier());
+        logic=new NOTDLogic(1600,2,28);
+        assertEquals(73,logic.howManyLeapYearsEarlier());
+    }
 }
